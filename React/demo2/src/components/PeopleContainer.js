0aa79bf0f2b5d9e27a69c179decd82b64c510eef
maksim <maksimfhmad@gmail.com>
2024-03-06 10:47:41 +0100

    last commit

--- React/demo2/src/components/PeopleContainer.js
+++ React/demo2/src/components/PeopleContainer.js
@@ -4,6 +4,10 @@ import Card from './Card';
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
+    const filterPeople = (filter) => {
+        let filtered = people.filter(person => person.name.includes(filter));
+        setPeople(filtered);
+    }
 
     useEffect(() => {
         fetch("https://65b3b754770d43aba47a4b3e.mockapi.io/person").then(
@@ -16,7 +20,12 @@ export default function PeopleContainer() {
 
     return (
         <div>
-            <h1>People</h1>
+            <div className=''>
+                <input className='border-2 border-black p-4 mt-6 ml-6' type='text' placeholder='Search' onChange={(el) => {
+                    filterPeople(el.target.value)
+                }} />
+            </div>
+
             <div className='grid grid-cols-4 p-10'>
                 {people.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle} />
